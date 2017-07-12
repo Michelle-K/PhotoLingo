a8bcecf4bab8028ccc0b5087cc4f6d7a2009b2cd
Michelle Kidd <michekidd@gmail.com>
2017-07-12 09:50:51 -0500

    initial

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+
+
+$("#go").on("click", function(){
+	//Save the name feild to firebase
+	var name = $("#name").val();
+	
+	$(".screen1").addClass("rotateOutUpRight");
+	$(".name").hide();
+
+	
+});
