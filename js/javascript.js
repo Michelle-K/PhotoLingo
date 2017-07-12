8924ca8e75220d9d27bd75f1c177c060f6b22227
Michelle Kidd <michekidd@gmail.com>
2017-07-12 10:41:01 -0500

    camera buttons

--- js/javascript.js
+++ js/javascript.js
@@ -2,10 +2,14 @@
 
 $("#go").on("click", function(){
 	//Save the name feild to firebase
-	var name = $("#name").val();
+	var name = $("#name").val();  //////////////Save Variable in firebase!
 	
 	$(".screen1").addClass("rotateOutUpRight");
 	$(".name").hide();
+	$(".screen1").hide();
+	$("#camera").show();
+	$("#camera").addClass("bounceIn");
+	$("#screen2text").show();
+	$("#screen2text").addClass("bounceIn");
 
-	
 });
