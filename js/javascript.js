86a751af4a6a05dec1639a84d53e06a9cffc6ba3
Michelle Kidd <michekidd@gmail.com>
2017-07-14 12:16:38 -0500

    class updates

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,37 @@
+//NAVBAR
+$(function () {
+    $('.navbar-toggler').on('click', function(event) {
+    	event.preventDefault();
+		$(this).closest('.navbar-minimal').toggleClass('open');
+	});
+});
+function htmlbodyHeightUpdate(){
+    	var height3 = $( window ).height();
+		var height1 = $('.nav').height()+50;
+		height2 = $('.main').height();
+		if(height2 > height3){
+			$('html').height(Math.max(height1,height3,height2)+10);
+			$('body').height(Math.max(height1,height3,height2)+10);
+		}
+		else
+		{
+			$('html').height(Math.max(height1,height3,height2));
+			$('body').height(Math.max(height1,height3,height2));
+		}
+		
+	}
+	$(document).ready(function () {
+		htmlbodyHeightUpdate();
+		$( window ).resize(function() {
+			htmlbodyHeightUpdate();
+		});
+		$( window ).scroll(function() {
+			height2 = $('.main').height();
+  			htmlbodyHeightUpdate();
+		});
+	});
+
+
 
 //First screen on click
 $("#go").on("click", function(){
@@ -40,23 +74,46 @@ function readURL(){
 }
 
 //If you click a word to learn the rest disappear
+var currentWord="";
+
 $("#word1").on("click", function(){
 	$("#word2,#word3,#word4").addClass("slideOutLeft");
 	$("#word2,#word3,#word4").hide(2000);
+	$("#spanishWord").show();
+	currentWord=$("#w1selected").html();
 });
 $("#word2").on("click", function(){
 	$("#word1,#word3,#word4").addClass("slideOutLeft");
 	$("#word1,#word3,#word4").hide(2000);
+	$("#spanishWord").show();
+	currentWord=$("#w2selected").html();
 });
 $("#word3").on("click", function(){
 	$("#word2,#word1,#word4").addClass("slideOutLeft");
 	$("#word2,#word1,#word4").hide(2000);
+	$("#spanishWord").show();
+	currentWord=$("#w3selected").html();
 });
 $("#word4").on("click", function(){
 	$("#word2,#word3,#word1").addClass("slideOutLeft");
 	$("#word2,#word3,#word1").hide(2000);
+	$("#spanishWord").show();
+	currentWord=$("#w4selected").html();
 });
 
+//Text to Speech -VOICERRS
+//Speak translated spanish word after you have clicked on the english word
 
 
+function runTranslator(){
+$.speech({
+            key: '<Ae7981639b98844409e5bde6150d7643c>',
+            src: 'Hello, world!',
+            hl: 'en-us',
+            r: 0, 
+            c: 'mp3',
+            f: '44khz_16bit_stereo',
+            ssml: false
+        });
 
+}
